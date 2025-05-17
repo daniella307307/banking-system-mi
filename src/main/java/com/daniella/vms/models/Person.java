cfcb2f0b36bb986b9659e13a515ef1ee0633f80c
daniella307307 <daniellaganza30@gmail.com>
2025-05-17 15:47:49 +0200

    first commit

---
+++ src/main/java/com/daniella/vms/models/Person.java
@@ -0,0 +1,33 @@
+package com.daniella.vms.models;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@MappedSuperclass
+@SuperBuilder
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    private String firstname;
+    private String lastname;
+    private String phoneNumber;
+    private LocalDate dob;
+    private Person(String firstname,String lastname,String phoneNumber){
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.phoneNumber=phoneNumber;
+    }
+
+}
